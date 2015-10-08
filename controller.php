37de210b1d0bf7616646d0cc91a4c7717ec9445f
Pavel Yakimov <moff.dev@gmail.com>
2015-10-08 22:47:04 +0700

    Changed icon

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 
     protected $pkgHandle = 'markdown_converter';
     protected $appVersionRequired = '5.7.3.1';
-    protected $pkgVersion = '0.9';
+    protected $pkgVersion = '0.9.1';
 
     public function getPackageName()
     {
